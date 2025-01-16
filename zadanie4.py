795cb73cafeef47c5685b5ce2fa3addca820cef3
MarekPo <131866939+Markowskky@users.noreply.github.com>
2025-01-16 10:26:08 +0100

    Add files via upload

---
+++ zadanie4.py
@@ -0,0 +1,23 @@
+file = open('liczby_przyklad.txt', 'r')
+
+s1 = file.readline()
+s2 = file.readline()
+
+l11 = s1.split(' ')
+l22 = s2.split(' ')
+
+l1 = list(map(int, l11))
+l2 = list(map(int, l22))
+l3 =
+
+print(s1)
+print(s2)
+for n1 in l1:
+    is_div = False;
+    j = 0;
+    while (not is_div) and (j < 12.len(l2)):
+        n2 = l2[j]
+        if n2 % n1 == 0:
+            is_div = True;
+        if is_div:
+            counter += 1
